117fc55fa94edd7b5b563344aad75536b46d7a6f
Jakemessi <nanaveine@gmail.com>
2025-02-16 21:18:11 -0300

    Agora na página inicial ficará uma sinalização de qual aba está carregada Nas tabelas as descrições estão centralizadas

--- 2Trim_15_TrilhaDeAprendizado/js/sub-pages.js
+++ 2Trim_15_TrilhaDeAprendizado/js/sub-pages.js
@@ -1,10 +1,36 @@
 function subpages(cavalo){
     const rodape = document.getElementById('rodape');
+    const iniciobotao = document.getElementById('iniciobotao');
+    const projetobotao = document.getElementById('projetobotao');
+    const sobrebotao = document.getElementById('sobrebotao');
+    const conteudosbotao = document.getElementById('conteudosbotao');
     if(cavalo == 'sub-pages/projeto.html'){
         rodape.style.display = "none";
+        projetobotao.style.color = "white";
+        iniciobotao.style.color = "darkgrey";
+        sobrebotao.style.color = "darkgrey";
+        conteudosbotao.style.color = "darkgrey";
     }
     else{
         rodape.style.display = "block";
+    }
+    if(cavalo == 'sub-pages/inicio.html'){
+        iniciobotao.style.color = "white";
+        projetobotao.style.color = "darkgrey";
+        sobrebotao.style.color = "darkgrey";
+        conteudosbotao.style.color = "darkgrey";
+    }
+    else if(cavalo == 'sub-pages/sobre.html'){
+        sobrebotao.style.color = "white";
+        iniciobotao.style.color = "darkgrey";
+        projetobotao.style.color = "darkgrey";
+        conteudosbotao.style.color = "darkgrey";
+    }
+    else if(cavalo == 'sub-pages/conteudos.html'){
+        conteudosbotao.style.color = "white";
+        iniciobotao.style.color = "darkgrey";
+        projetobotao.style.color = "darkgrey";
+        sobrebotao.style.color = "darkgrey";
     }
     fetch(cavalo)
     .then(res => res.text())
